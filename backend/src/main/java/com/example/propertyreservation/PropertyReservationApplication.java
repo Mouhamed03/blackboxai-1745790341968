ba83bebdb6a7b2f8107a7b8f8d300ba60add5dbe
Mouhamed03 <119949558+Mouhamed03@users.noreply.github.com>
2025-04-27 21:45:53 +0000

    Add backend/src/main/java/com/example/propertyreservation/PropertyReservationApplication.java 1745790352178

---
+++ backend/src/main/java/com/example/propertyreservation/PropertyReservationApplication.java
@@ -0,0 +1,12 @@
+package com.example.propertyreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyReservationApplication.class, args);
+    }
+}
