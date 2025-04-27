b3d85045e3a7dee7eac09a210d0451ddbc1c00d6
Mouhamed03 <119949558+Mouhamed03@users.noreply.github.com>
2025-04-27 21:46:14 +0000

    Add backend/src/main/java/com/example/propertyreservation/repository/OwnerRepository.java 1745790373381

---
+++ backend/src/main/java/com/example/propertyreservation/repository/OwnerRepository.java
@@ -0,0 +1,9 @@
+package com.example.propertyreservation.repository;
+
+import com.example.propertyreservation.model.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+}
